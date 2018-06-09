c9c510141bed5511728b9cbef396e5fb5bb7f640
go4retro <go4retro@go4retro.com>
2018-06-08 22:40:11 -0500

    configurable working 1MHz/working 4MHz/partially working 5MHz compilation

--- hdl/CoPro09.v
+++ hdl/CoPro09.v
@@ -52,30 +52,77 @@ module CoPro09(
                //input sw0,
                output led,
                output [7:0]test
-              );
+              );
+
+
+//`define 5MHZ
+`define 4MHZ
+
+`ifdef 5MHZ
+parameter CTR_LEN =           5;
+parameter CLK_CFG =           2'b01;
+`define CLK_2_DEF             (ctr[5:0] == 5'b01001)
+//`define CLK_2_DEF             (ctr[5:1] == 4'b0100)
+`define CLK_Q_DEF             (ctr[2] ^ ctr[1])
+`define CLK_E_DEF             (ctr[2])
+`define CLK_FAST              clk_mult
+`else
+`ifdef 4MHZ
+parameter CTR_LEN =           5;
+parameter CLK_CFG =           2'b11;
+// 15nS RAM
+`define CLK_2_DEF             (ctr[5:0] == 5'b10000)
+// 25nS RAM
+//`define CLK_2_DEF             (ctr[5:1] == 4'b1000)
+// 55nS RAM
+//`define CLK_2_DEF             (ctr[5:2] == 3'b100)
+`define CLK_Q_DEF             (ctr[3] ^ ctr[2])
+`define CLK_E_DEF             (ctr[3])
+`define CLK_FAST              clk_mult
+`else
+parameter CTR_LEN =           2;
+parameter CLK_CFG =           2'b11;
+`define CLK_2_DEF             clock
+`define CLK_Q_DEF             !(ctr[2] ^ ctr[1])
+`define CLK_E_DEF             !clock
+`define CLK_FAST              dotclk
+`endif
+`endif
+
 wire ce_regbase;
 wire ce_config;
-reg [2:0]ctr;
+reg [CTR_LEN:0]ctr;
 reg clock_prev;
 reg [7:0]data_cpu_out;
 reg [7:0]data_mem_out;
 wire [6:0]page;
+wire clock2;
+wire [7:0]data_mem_saved;
+wire clock_fast;
 
-assign test[0] =              clock;assign test[1] =              dotclk;assign test[2] =              clock_q;assign test[3] =              clock_e;assign test[4] =              dotclk;assign test[5] =              dotclk;assign test[6] =              dotclk;assign test[7] =              dotclk;
+assign test[0] =              clock; 
+assign test[1] =              clock2;
+assign test[2] =              clock_q;
+assign test[3] =              clock_e;
+assign test[4] =              dotclk;
+assign test[5] =              dotclk;
+assign test[6] =              dotclk;
+assign test[7] =              dotclk;
 
 
+assign clock2 =            `CLK_2_DEF;
 assign ce_regbase =        !_io[2] & (address_cpu[7:4] == 4'hf) & (address_cpu[3:2] == 2'b11);
 assign ce_config =         ce_regbase & (address_cpu[1:0] == 0);
 assign ce_page =           ce_regbase & (address_cpu[1:0] == 1);
-//assign ce_task_page =      ce_regbase & (address_cpu[1:0] == 2);
-//assign ce_extra =          ce_regbase & (address_cpu[1:0] == 3);
-assign _enbus =            !(_reset_09 & _halt_09 & clock_e); // for now, it's active on low half of phi2
-assign clock_e =           !clock;
-assign clk_cfg =           2'b11; // *8
+assign _enbus =            !(_reset_09 & _halt_09 & clock_e);
+assign clock_q =           `CLK_Q_DEF;
+assign clock_e =           `CLK_E_DEF;
+
+assign clk_cfg =           CLK_CFG;
 assign _game =             'bz;
 assign _exrom =            'bz;
-assign data_cpu =          data_cpu_out;assign data_mem =          data_mem_out;
-assign clock_q =           (ctr[2] ^ ctr[1]);
+assign data_cpu =          data_cpu_out;
+assign data_mem =          data_mem_out;
 assign _irq_09 =           1;
 assign _nmi_09 =           1;
 assign _firq_09 =          1;
@@ -86,6 +133,15 @@ register #(.WIDTH(1))      reg_halt(clock, !_reset, ce_config & !r_w, data_cpu[6
 
 register #(.WIDTH(7))      reg_page(clock, !_reset, ce_page & !r_w, data_cpu[6:0], page);
 
+`ifdef 4MHZ
+register #(.WIDTH(8))      reg_saved(clock2, !_reset, !_io[1] & r_w, data_mem, data_mem_saved);
+`else
+`ifdef 5MHZ
+register #(.WIDTH(8))      reg_saved(clock2, !_reset, !_io[1] & r_w, data_mem, data_mem_saved);
+`else
+assign data_mem_saved =    data_mem;
+`endif
+`endif
 always @(*)
 begin
    if(clock_e)
@@ -96,9 +152,9 @@ end
 
 always @(*)
 begin
-   if(!_io[1] & clock)
+   if(!_io[1] & clock2)
       _ce_dat = 0;
-   else if(!clock)
+   else if(clock_e)
       _ce_dat = 0;
    else
       _ce_dat = 1;
@@ -107,14 +163,14 @@ end
 always @(*)
 begin
    if (clock & r_w & !_io[1])
-      data_cpu_out = data_mem;
+      data_cpu_out = data_mem_saved;
    else
       data_cpu_out = 8'bz;
 end
 
 always @(*)
 begin
-   if(clock & !r_w & !_io[1])
+   if(clock2 & !r_w & !_io[1])
       data_mem_out = data_cpu;
    else
       data_mem_out = 8'bz;
@@ -122,21 +178,21 @@ end
 
 always @(*)
 begin
-   if(clock & _io[1])
+   if(clock2)
       address_mem = {page, address_cpu[7:0]};
    else
       address_mem = {6'b0,13'bz};
 end
 
-always @(negedge dotclk)
+always @(negedge `CLK_FAST)
 begin
    clock_prev <= clock;
 end
 
-always @(posedge dotclk)
+always @(posedge `CLK_FAST)
 begin
-   if(!clock)
-      if(clock_prev)
+   if(clock)
+      if(!clock_prev)
          ctr <= 0;
       else
          ctr <= ctr + 1;
